5a170c9fc5c9c631c33f432e3f041bf3af65d201
kareem walid <kareemwalid063@gmail.com>
2018-09-02 20:47:26 +0200

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -10,8 +10,8 @@ Names=('kareem' 'ali' 'sami' 'omar' 'keko')
 ages=(21 15 9 67 39)
 #echo ${Names[0]} have ${ages[0]} , ${Names[1]} have ${ages[1]} , ${Names[2]} have ${ages[2]}
 #echo ${Names[@]} to print all or you can use * 
-Names[5]="mayada"
-Names[6]='rasha rizk'
+Names[5]="xxxxxx"
+Names[6]='kam'
 ages[5]='16'
 ages[5]='40'
 #echo ${Names[@]}
